4dcf4a170516e06b057dfc397836cee92fcebc65
Vibhoru08 <37913233+Vibhoru08@users.noreply.github.com>
2018-12-16 22:23:42 +0530

    about us

--- code/application/views/index.php
+++ code/application/views/index.php
@@ -24,13 +24,13 @@
 
   <div class="carousel-inner">
     <div class="item active">
-     <img src="<?php echo base_url();?>asset/forntend/images/banner.jpg" />
+     <img src="</*?php echo base_url();?*/>asset/forntend/images/banner.jpg" />
       	<div class="about_content">
 				<div class="content_w">
-				<p class="mar_b20"><img src="<?php echo base_url();?>asset/forntend/images/about_logo.png"></p>
+				<p class="mar_b20"><img src="</*?php echo base_url();?*/>asset/forntend/images/about_logo.png"></p>
 				Coinenthu is community-driven crypto reviews and rating platform. It allows the community to share their thoughts about various Cryptocurrencies.
-					<div class="mar_t40 "><a href="<?php echo base_url();?>digital-assets" class="btn btn-danger btn-lg font_s22 div">Digital Assets</a>
-					<a href="<?php echo base_url();?>ico-tracker" class="btn btn-danger btn-lg font_s22 div" style="margin-left:20px">Upcoming ICOs</a></div>
+					<div class="mar_t40 "><a href="</*?php echo base_url();?*/>digital-assets" class="btn btn-danger btn-lg font_s22 div">Digital Assets</a>
+					<a href="</*?php echo base_url();?*/>ico-tracker" class="btn btn-danger btn-lg font_s22 div" style="margin-left:20px">Upcoming ICOs</a></div>
 				</div>
 		</div>
     </div>
@@ -44,10 +44,30 @@
 </div>
 
 </div> -->
-			<div class="container-fluid about">
+			<div class="container-fluid about mar_b120">
 				<div class="row">
-				<div class="text-center" style="margin-bottom:50px;"><h2 class="mar_b30 div1 div2"><span style="padding-bottom:5px;">WHY COINENTHU?<hr style="width:5%;border:1px solid #404852;margin-top:5px;"></span></h2>
-					<p class = "div1" style="font-size:24px;margin-bottom:20px;">Over the past few years, we understand that a lot of newcomers do not have a direction to research various crypto projects. There is a lot of information out there which can get intimidating. That's where CoinEnthu comes in, by the form of ratings and reviews that's easily interpretable. We encourage you to take a look at what crypto veterans are saying about each project before investing blindly.</p>
+				<div class="text-center" style="margin-bottom:50px;"><h2 class="mar_b30 div1 div2"><span style="padding-bottom:5px;">WHY COINENTHU?<hr style="width:5%;color:#404852;border:1px solid #404852;margin-top:5px;"></span></h2>
+					<!--<p class = "div1" style="font-size:24px;margin-bottom:20px;">Over the past few years, we understand that a lot of newcomers do not have a direction to research various crypto projects. There is a lot of information out there which can get intimidating. That's where CoinEnthu comes in, by the form of ratings and reviews that's easily interpretable. We encourage you to take a look at what crypto veterans are saying about each project before investing blindly.</p>-->
+				</div>
+				</div>
+				<div class = "row mar_t80">
+				<div class ="col-md-3 col-md-offset-1 mar_r65 col-xs-6">
+				<div class = "new_boxes text-justify box_padding">
+				<img src = "<?php echo base_url().'asset/img/gear.png'; ?>" alt= "settings icon" class="img-circle about_box_image">		
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad consequat. sequat. 
+				</div>
+				</div>
+				<div class ="col-md-3 mar_r65 col-xs-6">
+				<div class = "new_boxes text-justify box_padding">
+				<img src = "<?php echo base_url().'asset/img/checklist.png'; ?>" alt= "checklist icon" class="img-circle about_box_image">	
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad consequat. sequat. 
+				</div>
+				</div>
+				<div class ="col-md-3 col-xs-6">
+				<div class = "new_boxes text-justify box_padding">
+				<img src = "<?php echo base_url().'asset/img/badge.png'; ?>" alt= "checklist icon" class="img-circle about_box_image">		
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad consequat. sequat. 
+				</div>
 				</div>
 				</div>
 			</div>
