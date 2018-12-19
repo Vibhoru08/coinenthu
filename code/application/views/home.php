8d93f9bfa85b10a0be27f0b78e86a3207ba9b762
pr0-gmaer <akrchaubey@gmail.com>
2018-12-19 21:11:35 +0530

    home page

--- code/application/views/home.php
+++ code/application/views/home.php
@@ -7,27 +7,34 @@
                     <hr style="width:5%;border:1px solid #ffff">
                 </div>
             </div>
-            <div class = "row text-center">    
+            <div class = "row text-center">
                 <div class = "col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2 banner_desc">
                     Let's not invest blindly, get the correct information on your Cryptos.<br/> Search for them.
-                </div>    
+                </div>
             </div>
             <div class = "row text-center mar_t30 mar_b50">
                 <div class="col-sm-5 col-sm-offset-3 col-xs-5 col-xs-offset-2">
-                    <input class="form-control1 form-control mar_r10" type="text" onkeyup="sreachterm();" name="searchhome" id="searchhome" placeholder="&nbsp;&#xF002; &nbsp;&nbsp;&nbsp;Search for your Digital Assets and ICOs" style="font-family:NoirPro, FontAwesome;font-size: 16px;font-weight: 300;font-style: italic;color: #231f20;" />
+                    <input class="form-control1 form-control mar_r10 searchhome" type="text" onkeyup="sreachterm();" name="searchterms" id="searchterms" placeholder="&nbsp;&#xF002; &nbsp;&nbsp;&nbsp;Search for your Digital Assets and ICOs"  />
                 </div>
                 <div class = "col-sm-2 col-xs-2" style = "width:150px;">
                     <button class="btn btn-success" type = "button" style = "width:100%">Search</button>
-                </div>     
-            </div>    
+                </div>
+            </div>
         </div>
         <div class= "row text-center mar_t70">
             <div class = "col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2">
                 <p class = "home_desc">
                     Come here and discover the Cryptos you are interested about!<br/>
                     Some of the most reviewed Cryptos and ICOs are:
-                </p>    
-            </div>    
-        </div>    
+                </p>
+            </div>
+        </div>
     </section>
-</div>    
+</div>
+<script>
+$( "#searchterms" ).keypress(function( event ) {
+  if ( event.which == 13 ) {
+    sreachterm();
+  }
+});
+</script>
