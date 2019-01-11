2f0b081d4b03f9b07f25f1ceac72e55afb5ab312
pr0-gmaer <akrchaubey@gmail.com>
2019-01-11 18:53:25 +0530

    Update ico-tracker.php

--- code/application/views/ico-tracker.php
+++ code/application/views/ico-tracker.php
@@ -6,10 +6,10 @@ $viewTime = date('Ymd') .'_'. date('His');
 
 	<!-- <div  style="border-bottom:1px solid #ddd"> -->
   <div class="container-fluid banner_margin linear_color">
-    <div class="row mmar_t40 mmar_b10 mar_t70 mar_b80">
+    <div class="row mmar_t40 mmar_b10 mar_t30 mar_b40">
       <div class="col-xs-12 text-center banner_head">
         ICO TRACKER
-        <hr style="width:5%;border:2px solid #ffff">
+      <!--  <hr style="width:5%;border:2px solid #ffff">-->
         <div class="banner_desc">
         Let’s not invest blindly, get the correct information on your ICO TRACKERS.
         <div>Search for them.</div>
@@ -38,7 +38,7 @@ $viewTime = date('Ymd') .'_'. date('His');
 			<div class="box mar_b5 sorting home_box_n">
 				<div class="box-header pad_0">
 				<div class="row">
-					<div class="col-md-3 col-md-offset-1 col-sm-4 col-sm-offset-4 col-xs-12 col-xs-offset-0 msearch_bg pad_0">
+					<div class="col-md-5 col-md-offset-1 col-sm-4 col-sm-offset-4 col-xs-12 col-xs-offset-0 msearch_bg">
             <div class = "row s_width">
 						<div class = "col-md-8 col-sm-9 col-sm-offset-1 col-md-offset-0 col-xs-6 col-xs-offset-2  ss_width">
 						<input class="form-control brg_focus_n" onkeyup="sreachterm();" type="text" name="searchterms" id="searchterms" placeholder="Search ICOs">
@@ -48,7 +48,7 @@ $viewTime = date('Ymd') .'_'. date('His');
         </div>
           </div>
         </div>
-        <div class="col-md-3 col-md-offset-1 col-sm-4 col-sm-offset-4 col-xs-12 col-xs-offset-0 msearch_bg mpad_b3 pad_l0 a_width" >
+        <div class="col-md-3 col-md-offset-2 col-sm-4 col-sm-offset-4 col-xs-12 col-xs-offset-0 msearch_bg mpad_b3 pad_l0 a_width" >
           <div class="row">
             <div class="col-md-8 col-sm-9 col-sm-offset-1 col-md-offset-0 col-xs-12 col-xs-offset-0 centered ">
 						<div class="select_style">
@@ -82,14 +82,14 @@ $viewTime = date('Ymd') .'_'. date('His');
 </div>
 </div>
 <div class = "row">
-	<div class = "col-md-8">
+	<div class = "col-md-10 col-md-offset-1">
 			<input type="hidden" id="totcntcompanies" value="<?php echo $totCntIcos; ?>" />
 			<input type="hidden" id="limitpage"  value="12" />
 			<input type="hidden" id="offsetpage" value="12" />
 			<input type="hidden" id="pageMode" value="icos" />
 			<div class="row company_list">
 			<?php if(sizeof($icoTrackers)>0){ foreach($icoTrackers as $key=>$value){?>
-				<div class="col-md-5 col-md-offset-1">
+				<div class="col-md-4 col-md-offset-0">
 					<ul class="products-list product-list-in-box">
 						<li class="item center">
 						<div class="product_zorder">
@@ -146,15 +146,15 @@ $viewTime = date('Ymd') .'_'. date('His');
 			<span id="m_hide"><br/></span>
 			<?php } ?>
 			</div>
-			<div class="col-md-3 mar_t86">
+		<!--	<div class="col-md-3 mar_t86">
 				<div class="new_boxes upcoming_box_padding">
 					<div class = "text-center">
 						<h4>UPCOMING EVENTS</h4>
 					</div>
 					<div>
-					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+					<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">-->
 					  <!-- Indicators -->
-						<ol class="carousel-indicators">
+					<!--	<ol class="carousel-indicators">
 						<?php
 						if(count($digitalAssetsImages) > 0)
 						{
@@ -217,14 +217,14 @@ $viewTime = date('Ymd') .'_'. date('His');
 							</div>
 							<?php }?>
 						</div>
-						 <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+						 <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">-->
 						<!--<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 						<span class="sr-only">Previous</span>-->
-					  </a>
-					  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+					 <!-- </a>
+					  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">-->
 						<!--<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 						<span class="sr-only">Next</span>-->
-					  </a>
+					 <!-- </a>
 						</div>
 					</div>
 				</div>
@@ -236,7 +236,7 @@ $viewTime = date('Ymd') .'_'. date('His');
 						</div>
 					</div>
 				</div>
-			</div>
+			</div>-->
 		</div>
 	  </section>
 	  <div class="mar_b40"></div>
